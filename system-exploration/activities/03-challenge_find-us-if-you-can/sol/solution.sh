aaeb01bf9bdc3f02a23e5a3cd9820b2381e5dba9
teo <teodor.dutu@gmail.com>
2021-08-27 20:30:57 +0300

    Add system-exploration session content

---
+++ system-exploration/activities/03-challenge_find-us-if-you-can/sol/solution.sh
@@ -0,0 +1,7 @@
+echo "Get the first flag."
+find / -type f -name flag
+cat /proc/1/net/stat/flag | grep SSS
+
+echo "Now get the second one."
+find / -name *doc* | grep bugs
+cat /tmp/.hidden/my/bugs/bunny/bugs_bunny.doc | grep SSS
